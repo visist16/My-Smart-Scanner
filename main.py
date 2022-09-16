2ed227081dce2b90fada33a09fd9cff9b003a8b8
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-09-16 23:52:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import cv2
 from PIL import Image
 import time
 url = 'http://192.168.1.90:8080/shot.jpg'
+while True:
+    img = request.urlopen(url)
+    img_bytes = bytearray(img.read())
+    img_np = np.array(img_bytes, dtype=np.uint8)
+    frame = cv2.imdecode(img_np, -1)
